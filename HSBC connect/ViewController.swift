354ba4f712ac23a454d5889721689997c6df1b43
Ibrahim Kamara <ibrahimkamara@live.co.uk>
2022-11-14 12:25:09 +0000

    Initial Commit

---
+++ HSBC connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HSBC connect
+//
+//  Created by Ibrahim Kamara on 10/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
